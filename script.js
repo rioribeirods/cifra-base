cef775ec55d7cb5336073dc90411ec0fe47df4d6
Rio Ribeiro <rioribeirods@gmail.com>
2022-05-19 19:05:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+
+
+let decode = document.querySelector('#decode')
+let encode = document.querySelector('#encode')
+let result = document.querySelector('#result')
+let textArea = document.querySelector('#textArea')
+let cifra = document.querySelector('#cifra')
+let base = document.querySelector('#base')
+let button = document.querySelector('#button')
+let method
+let incremento = 1
+
+
+let selection = document.querySelector('#selection')
+let hiddenInput = document.querySelector('#hiddenInput')
+// Function pra pegar entrada do usuário
+selection.addEventListener("change", function(selection) {
+    var selectionEscolhida = selection.target.value
+
+        if(selectionEscolhida == 'base64') {
+            hiddenInput.style.display = "none"
+            console.log("teste")
+            button.setAttribute("onclick", "base64()")
+        } else {
+            hiddenInput.style.display = "inline"
+            button.setAttribute("onclick", "cesar()")
+        }
+    }
+)
+
+encode.addEventListener("click", function () {
+    button.innerText = "Codificar";
+    console.log("encode teste")
+})
+
+decode.addEventListener("click", function () {
+    button.innerText = "Decodificar";
+})
+
+function base64(){
+    let mensagem = textArea.value
+    let choice = encode.selected
+    console.log('base64teste')
+    result.value = base64Logic(mensagem, choice);
+}
+
+function base64Logic(mensagem, choice){
+    console.log('base64logicteste')
+    return (choice)? btoa(mensagem) : atob(mensagem);
+  }
+
+function cesar()  {
+    let mensagem = textArea.value.split("");
+    var choice = encode.checked
+    var numero =  parseInt(hiddenInput.value);
+    if (choice){
+      result.value = cesarEncode(mensagem, numero);
+    } 
+    else {
+      result.value = cesarDecode(mensagem, numero);
+    }
+  }
+  
+  
+  function cesarEncode(arr, key) {
+    return arr.map((c)=>{
+        let code = c.charCodeAt();
+        if(code >= 65 && code <= 90){
+            return String.fromCharCode(((code - 65 + key) % 26) + 65) 
+        } else if(code >= 97 && code <= 122){
+            return String.fromCharCode(((code - 97 + key) % 26) + 97)
+        } else return c
+    }).join('') 
+  }
+  
+  function cesarDecode(arr, key){
+    return arr.map((c)=>{
+        let code = c.charCodeAt();
+        if(code >= 65 && code <= 90){
+            return (code-65-key < 0)?String.fromCharCode(((code - 65 - key + 26)%26)+65):String.fromCharCode(((code - 65 - key)%26)+65) 
+        } else if(code >= 97 && code <= 122){
+            return String.fromCharCode(((code - 97 - key + 26) % 26) + 97)
+        } else return c
+    }).join('')
+ }
